9f682a5db961ea5b6e9e1ad84cc66259df57d2d6
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-28 01:44:07 +0300

    Add test for post

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe User , type: :model do
   it '@user created should be valid' do
     expect(@user).to be_valid
   end
+  it 'name value should be present' do
+    @user.name = nil
+    expect(@user).to_not be_valid
+  end
+  it 'posts_counter must have a numeric value' do
+    @user.post_counter = 'abc'
+    expect(@user).to_not be_valid
+  end
+
+  it 'post_counter must be >= 0' do
+    @user.post_counter = -1
+    expect(@user).to_not be_valid
+  end
+
+  it 'has a valid bio' do
+    @user.bio = 'nil'
+    expect(@user).to_not be_valid
+  end
+
 end
