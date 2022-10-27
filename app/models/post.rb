805dbf64ceff173c8c16f3a5dc16b792d477ba69
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-27 23:32:32 +0300

    Add comments and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,13 @@
 class Post < ApplicationRecord
-    has_many :posts, foreign_key: 'author_id'
-    has_many :comments, foreign_key: 'author_id'
-    has_many :likes, foreign_key: 'author_id'
-    has_many :posts, foreign_key: 'author_id'
-    has_many :comments, foreign_key: 'author_id'
-    has_many :likes, foreign_key: 'author_id'
-
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
   def update_post_counter
-      author.increment!(:posts_counter)
+    author.increment!(:posts_counter)
   end
 
   def recent_comments
     comments.order(created_at: DESC).limit(5)
   end
-
 end
+
