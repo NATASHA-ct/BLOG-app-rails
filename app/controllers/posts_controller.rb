a95ab215fbd8815c1c0f77cdc5424ab4dab58f93
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-10-31 21:45:18 +0200

    Add comments to controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index; end
 
+# view a specific post  
   def show
     @post_id = params[:id]
   end
--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -1,6 +1,7 @@
 class UsersControllers < ApplicationController
   def index; end
 
+  # view a specific user  
   def show
     params[:users_id]
   end
