e5fb59a3f61b5a20ec17d8f6f9f8b94038424f1d
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-11-03 01:47:59 +0200

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
-   @posts = Post.all
-   @user = User.includes(posts: [:comments]).find(params[:user_id].to_i)
+    @posts = Post.all
+    @user = User.includes(posts: [:comments]).find(params[:user_id].to_i)
   end
 
   def show
-    @post =  Post.find(params[:id])
+    @post = Post.find(params[:id])
     @comments = @post.comments
   end
 end
