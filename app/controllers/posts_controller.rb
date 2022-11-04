d3e984f4c3cd689725b822876a40402f662a740c
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-11-04 22:37:49 +0200

    Add new and create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,29 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    @post = Post.new(post_params)
+    @author = current_user
+    @post.author = @author
+    if @post.save
+      flash[:success] = 'Post was successfully created'
+      redirect_to new_user_post_path(@author.id)
+    else
+      flash[:error] = 'Error:  Post was not created!!'
+      render :new
+    end
+  end
+
+  private
+
+  #strong parameters
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
