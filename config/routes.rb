7bffef796dbb681ba8b8fca1004f4f795983a8af
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-11-04 22:23:24 +0200

    Create a method called current_user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
     root 'users#index'
     resources :users, only: [:index, :show] do
       resources :posts, only: [:index,:show] 
+      resources :posts, only: [:index, :new, :create, :show ] 
+        resources :comments, only: [:create]
+        resources :likes, only: [:create]
     end
 end
