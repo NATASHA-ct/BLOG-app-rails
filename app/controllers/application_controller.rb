f0197f0a427dc8c00e82af2d33e188335adae10a
karlavdelgadof <karlavdelgadof@gmail.com>
2022-11-10 14:58:09 -0500

    Add sign up parameters config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_admin!
-    protect_from_forgery with: :exception
-    before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_admin!
+  protect_from_forgery with: :exception
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:Name, :email, :password) }
-      devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:Name, :Bio, :Photo, :email, :password, :current_password)
-      end
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:Name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:Name, :Bio, :Photo, :email, :password, :current_password)
     end
+  end
 end
