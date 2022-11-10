220e4ee654c6746854da1e1fabcf239eed95fec1
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-11-10 14:45:56 +0200

    Delete current user method from app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    User.find(params[:user_id])
-  end
+    before_action :authenticate_user!
+    protect_from_forgery with: :exception
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+    protected
+
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:Name, :Bio, :Photo, :email, :password) }
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:Name, :Bio, :Photo, :email, :password, :current_password)
+      end
+    end
 end
