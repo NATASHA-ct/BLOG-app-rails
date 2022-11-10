cbd2d5a9dcfa583ff023996bb473026016eea455
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-11-10 19:57:59 +0200

    Modify app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,1 @@
-class ApplicationController < ActionController::Base
-    before_action :authenticate_admin!
-    protect_from_forgery with: :exception
-    before_action :update_allowed_parameters, if: :devise_controller?
 
-    protected
-
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:Name, :email, :password) }
-      devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:Name, :Bio, :Photo, :email, :password, :current_password)
-      end
-    end
-end
