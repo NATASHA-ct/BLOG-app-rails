bf2387a2aadfd9b09c1a19f3aca9559408bf9e9c
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-11-10 14:47:32 +0200

    Delete bio and phot from sign up parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:Name, :Bio, :Photo, :email, :password) }
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:Name, :email, :password) }
       devise_parameter_sanitizer.permit(:account_update) do |u|
         u.permit(:Name, :Bio, :Photo, :email, :password, :current_password)
       end
