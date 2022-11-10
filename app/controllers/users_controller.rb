bcec097383384dc488c00f6fb1841c28962587fa
karlavdelgadof <karlavdelgadof@gmail.com>
2022-11-10 14:55:47 -0500

    Remove uncommented code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
-  
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    # @posts = @user.posts
   end
 end
