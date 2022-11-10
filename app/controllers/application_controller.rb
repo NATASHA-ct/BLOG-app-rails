578efe9cd858069cd4791b3d473faea4d4115f25
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-11-10 15:44:58 +0200

    Fix log in error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+    before_action :authenticate_admin!
     protect_from_forgery with: :exception
     before_action :update_allowed_parameters, if: :devise_controller?
 
